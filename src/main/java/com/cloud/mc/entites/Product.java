cdd21c2c64893255c2544d5ee117469cc34c6b29
joomoraes <52724220+joomoraes@users.noreply.github.com>
2021-02-14 17:21:21 -0300

    Criação da entidade product

--- src/main/java/com/cloud/mc/entites/Product.java
+++ src/main/java/com/cloud/mc/entites/Product.java
@@ -12,24 +12,31 @@ import javax.persistence.Table;
 import javax.persistence.Transient;
 
 @Entity
-@Table(name = "tb_category")
-public class Category implements Serializable {
+@Table(name = "tb_product")
+public class Product implements Serializable {
+	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	private String Description;
+	private Double price;
+	private String ImgUrl;
 	
 	@Transient
-	private Set<Product> products = new HashSet<>();
+	private Set<Category> categories = new HashSet<>();
 	
-	public Category() {}
+	public Product() {}
 
-	public Category(Long id, String name) {
+	public Product(Long id, String name, String description, Double price, String imgUrl) {
 		super();
 		this.id = id;
 		this.name = name;
+		Description = description;
+		this.price = price;
+		ImgUrl = imgUrl;
 	}
 
 	public Long getId() {
@@ -47,11 +54,34 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public Set<Product> getProducts() {
-		return products;
+
+	public String getDescription() {
+		return Description;
 	}
 
+	public void setDescription(String description) {
+		Description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return ImgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		ImgUrl = imgUrl;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
 
 	@Override
 	public int hashCode() {
@@ -69,7 +99,7 @@ public class Category implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -77,6 +107,5 @@ public class Category implements Serializable {
 			return false;
 		return true;
 	}
-
 	
 }
