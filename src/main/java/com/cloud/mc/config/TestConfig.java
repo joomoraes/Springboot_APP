7eeba79ccef19971576f3dfa8ca7d21a2ef277e7
joomoraes <52724220+joomoraes@users.noreply.github.com>
2021-02-16 19:17:32 -0300

    Criação de entidade Payment e associação dela de um para um

--- src/main/java/com/cloud/mc/config/TestConfig.java
+++ src/main/java/com/cloud/mc/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.cloud.mc.entites.Category;
 import com.cloud.mc.entites.Order;
 import com.cloud.mc.entites.OrderItem;
+import com.cloud.mc.entites.Payment;
 import com.cloud.mc.entites.Product;
 import com.cloud.mc.entites.User;
 import com.cloud.mc.entites.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
