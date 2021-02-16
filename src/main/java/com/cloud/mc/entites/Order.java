8ce7bfc4a016de73f689737625d1695c89c8a898
joomoraes <52724220+joomoraes@users.noreply.github.com>
2021-02-16 19:53:09 -0300

    Atribuição de método total e subtotal

--- src/main/java/com/cloud/mc/entites/Order.java
+++ src/main/java/com/cloud/mc/entites/Order.java
@@ -101,6 +101,15 @@ public class Order implements Serializable {
 	public Set<OrderItem> getItems() {
 		return items;
 	}
+	
+	public Double getTotal() {
+		double sum = 0.0;
+		for (OrderItem x : items) {
+			sum += x.getSubTotal(); 
+		}
+		return sum;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
