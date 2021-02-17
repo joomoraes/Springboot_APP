2b11c5f03803f467cba85af3a00a7cf38ccaf84f
joomoraes <52724220+joomoraes@users.noreply.github.com>
2021-02-17 19:12:52 -0300

    Criação de tratamento de exceção para o FindById

--- src/main/java/com/cloud/mc/service/UserService.java
+++ src/main/java/com/cloud/mc/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 
 import com.cloud.mc.entites.User;
 import com.cloud.mc.repositoryes.UserRepository;
+import com.cloud.mc.service.exception.ResourceNotFoundException;
 
 
 @Component
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id)) ;
 	}
 	
 	public User insert(User obj) {
